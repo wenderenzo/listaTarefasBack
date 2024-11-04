77a15b6b64e86dbbe711d951f7b7f98ce2f7663e
Wender Enzo <wenderenzo2014@outlook.com>
2024-11-04 11:27:03 -0300

    fix. add new local for listen in .allowedOrigins https://listatarefasback.onrender.com

--- src/main/java/com/example/tarefas/config/WebConfig.java
+++ src/main/java/com/example/tarefas/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000" , "https://listatarefas-uzhi.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
